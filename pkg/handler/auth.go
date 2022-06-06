0a115f85b315d09823c91c5cdf65e0cefcb36f1d
Sammy10579 <ramenskiivlad@gmail.com>
2022-06-06 21:14:44 +0300

    added register users

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,27 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"todoApp"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todoApp.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.service.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) singIn(c *gin.Context) {
